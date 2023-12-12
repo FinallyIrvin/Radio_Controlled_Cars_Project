ebe82b2f44f6c79fa8357c97f26c31ad5dc0982e
Irvin Varela <irvinvarela11@gmail.com>
2023-12-11 22:31:24 -0600

    AbstractCarOptions

---
+++ src/main/java/com/rccars/model/AbstractCarOptions.java
@@ -0,0 +1,16 @@
+package com.rccars.model;
+
+import java.math.BigDecimal;
+
+public abstract class AbstractCarOptions extends AbstractCar {
+
+    protected AbstractCar customCar;
+
+    public AbstractCarOptions(AbstractCar car) {
+        super(car.getShellColor(), car.getBodyShell(), car.getModelNumber());
+        customCar = car;
+    }
+
+    public abstract String getOptionName();
+    public abstract BigDecimal getOptionAmount();
+}
