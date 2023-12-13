2ea1d99eda58841e3c1a734c66ab13d889f757a4
Alfredo Jurado <fredointech5@gmail.com>
2023-12-12 21:17:58 -0600

    update draft sample

--- src/main/java/com/rccars/main/SampleOrder.java
+++ src/main/java/com/rccars/main/SampleOrder.java
@@ -41,8 +41,45 @@ public class SampleOrder {
 
     private void buyCar() {
         System.out.println("What kind of car would interest you?");
-        System.out.println("Your options are:\nc - Classic\n" +
-                "d - DuneBuggy\nc - Crawler\na - ATV ");
+        System.out.println("Your options are:\n1 - Classic\n" +
+                "2 - DuneBuggy\n3 - Crawler\n4 - ATV\n5 - Sport\n6 - SUV");
+        String type = userIn.next();
+        validateCarType(type);
+        frameTypeOffer(carChoice);
+        offerUpgrades();
+        carOrder.add(carChoice);
+        anotherCar();
+        orderDone();
+    }
+
+    private void orderDone() {
+        new SampleReceipt(carOrder, customOptionsList, acessoryOrder);
+
+
+    private void anotherCar() {
+        System.out.println("Would you like to add another car? yes or no");
+            String answer = userIn.next();
+            validateAnotherCar(answer);
+        }
+
+        private void offerUpgrades() {
+            System.out.println("Apart from the frame options," +
+                    " we also offer tire upgrades.");
+            System.out.println("The following tire upgrades are:\n" +
+                    "1 - full spiked tires\n2 - slick tires\n3 - no upgrades");
+            String upgrade = userIn.next();
+            validateUpgrades(upgrade);
+            moreUpgrades();
+        }
+
+        private void frameTypeOffer(AbstractCar car) {
+            System.out.println("Our cars come with a" +
+            " default frame.");
+            System.out.println("Your current frame" +
+                    " is "+ (car.get);
+    }
+    
+
 
     }
 
